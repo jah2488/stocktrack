221d07545ea6acc4b15a6a9ff2537764ae529cfb
mattrice12 <mattrice12@outlook.com>
2020-09-06 18:57:39 -0500

    Devise, Omniauth, basic styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 # typed: strict
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :request_basic_auth
+
+  def request_basic_auth
+    unless ENV['BASIC_AUTH_USERNAME'].blank?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+      end
+    end
+  end
 end
